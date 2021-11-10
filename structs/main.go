2ec52944e03a188e312ccea3d4db8b569c9f0800
Pe-Guedss <pedro.og2002@gmail.com>
2021-11-10 14:44:26 -0300

    Adicionando uma interface para as áreas.

--- structs/main.go
+++ structs/main.go
@@ -12,6 +12,7 @@ type Circle struct {
 	coord_y  float32
 	radius   float64
 	area_cir float64
+	tipo     string
 }
 
 func (c Circle) construtor(num int) {
@@ -26,6 +27,7 @@ func (c *Circle) init() {
 	c.coord_x = rand.Float32() * float32(rand.Intn(100))
 	c.coord_y = rand.Float32() * float32(rand.Intn(100))
 	c.area_cir = c.area()
+	c.tipo = "Círculo"
 }
 
 func (c Circle) area() float64 {
@@ -45,20 +47,26 @@ type Rectangle struct {
 	coord_y float32
 	len     float64
 	wid     float64
+	tipo    string
 }
 
 func (r Rectangle) construtor(num int) {
-	for r.len == 0 || r.wid == 0 {
+	for true {
 		r.init()
+		fmt.Printf("len: %f  || wid: %f", r.len, r.wid)
+		if !(r.len == 0 || r.wid == 0) {
+			break
+		}
 	}
 	r.print(num)
 }
 
-func (r Rectangle) init() {
+func (r *Rectangle) init() {
 	r.coord_x = rand.Float32() * float32(rand.Intn(100))
 	r.coord_y = rand.Float32() * float32(rand.Intn(100))
 	r.len = rand.Float64() * float64(rand.Intn(10))
 	r.wid = rand.Float64() * float64(rand.Intn(10))
+	r.tipo = "Retângulo"
 }
 
 func (r Rectangle) area() float64 {
@@ -72,14 +80,20 @@ func (r Rectangle) print(num int) {
 	fmt.Printf("Área: %f\n\n", r.area())
 }
 
+//Interfaces
+type Shape interface {
+	area() float64
+}
+
 func main() {
 	num := int(0)
+	shapes := []Shape{}
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("\nOcorreu um erro (panic!) terminal!\n")
 		}
 		if num < 0 {
-			fmt.Println("\nO número não pode ser negativo.")
+			fmt.Printf("\nO número não pode ser negativo.\n")
 		}
 	}()
 
@@ -89,14 +103,21 @@ func main() {
 	for i := 0; i < num; i++ {
 		c[i].radius = 0
 		c[i].construtor(i)
+		shapes = append(shapes, c[i])
 	}
 
 	fmt.Print("Quantos retângulos você quer criar? ")
 	fmt.Scan(&num)
 	r := make([]Rectangle, num)
+	fmt.Print("a")
 	for i := 0; i < num; i++ {
 		r[i].len = 0
 		r[i].wid = 0
 		r[i].construtor(i)
+		shapes = append(shapes, r[i])
+	}
+
+	for _, shape := range shapes {
+		fmt.Println(shape.area())
 	}
 }
