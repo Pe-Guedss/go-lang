5118866f80e334d1b700ec112224b3de939aa67f
Pe-Guedss <pedro.og2002@gmail.com>
2021-11-10 18:08:12 -0300

    Programa para treinar estruturas em GO.

--- structs/main.go
+++ structs/main.go
@@ -12,7 +12,6 @@ type Circle struct {
 	coord_y  float32
 	radius   float64
 	area_cir float64
-	tipo     string
 }
 
 func (c Circle) construtor(num int) {
@@ -27,10 +26,9 @@ func (c *Circle) init() {
 	c.coord_x = rand.Float32() * float32(rand.Intn(100))
 	c.coord_y = rand.Float32() * float32(rand.Intn(100))
 	c.area_cir = c.area()
-	c.tipo = "Círculo"
 }
 
-func (c Circle) area() float64 {
+func (c *Circle) area() float64 {
 	return math.Pi * math.Pow(c.radius, 2)
 }
 
@@ -39,15 +37,16 @@ func (c Circle) print(num int) {
 	fmt.Printf("Raio: %f\n", c.radius)
 	fmt.Printf("X: %f  --  Y: %f\n", c.coord_x, c.coord_y)
 	fmt.Printf("Área: %f\n\n", c.area_cir)
+	sum_areas += c.area_cir
 }
 
 //Retângulos e quadrados
 type Rectangle struct {
-	coord_x float32
-	coord_y float32
-	len     float64
-	wid     float64
-	tipo    string
+	coord_x   float32
+	coord_y   float32
+	len       float64
+	wid       float64
+	area_rect float64
 }
 
 func (r Rectangle) construtor(num int) {
@@ -66,10 +65,10 @@ func (r *Rectangle) init() {
 	r.coord_y = rand.Float32() * float32(rand.Intn(100))
 	r.len = rand.Float64() * float64(rand.Intn(10))
 	r.wid = rand.Float64() * float64(rand.Intn(10))
-	r.tipo = "Retângulo"
+	r.area_rect = r.area()
 }
 
-func (r Rectangle) area() float64 {
+func (r *Rectangle) area() float64 {
 	return r.len * r.wid
 }
 
@@ -78,16 +77,14 @@ func (r Rectangle) print(num int) {
 	fmt.Printf("Comprimento: %f  --  Largura: %f\n", r.len, r.wid)
 	fmt.Printf("X inicial: %f  --  Y inicial: %f\n", r.coord_x, r.coord_y)
 	fmt.Printf("Área: %f\n\n", r.area())
+	sum_areas += r.area()
 }
 
-//Interfaces
-type Shape interface {
-	area() float64
-}
+var sum_areas float64
 
 func main() {
+	sum_areas = 0
 	num := int(0)
-	shapes := []Shape{}
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("\nOcorreu um erro (panic!) terminal!\n")
@@ -103,21 +100,16 @@ func main() {
 	for i := 0; i < num; i++ {
 		c[i].radius = 0
 		c[i].construtor(i)
-		shapes = append(shapes, c[i])
 	}
 
 	fmt.Print("Quantos retângulos você quer criar? ")
 	fmt.Scan(&num)
 	r := make([]Rectangle, num)
-	fmt.Print("a")
 	for i := 0; i < num; i++ {
 		r[i].len = 0
 		r[i].wid = 0
 		r[i].construtor(i)
-		shapes = append(shapes, r[i])
 	}
 
-	for _, shape := range shapes {
-		fmt.Println(shape.area())
-	}
+	fmt.Printf("A soma total das áreas é: %f", sum_areas)
 }
